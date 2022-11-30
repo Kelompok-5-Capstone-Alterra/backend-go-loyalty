1aedcf5ae548e4d0e9ee81e6f99ff38565eb7c67
Amandavara <amanda.vara.2002316@students.um.ac.id>
2022-11-30 17:30:06 +0700

    add: code refactoring

--- internal/controller/reward/reward_controller.go
+++ internal/controller/reward/reward_controller.go
@@ -1,76 +1,143 @@
 package controller
 
 import (
-	"backend-go-loyalty/internal/entity"
-	"encoding/json"
+	"backend-go-loyalty/internal/dto"
+	rewardService "backend-go-loyalty/internal/service/reward"
+	"backend-go-loyalty/pkg/response"
 	"net/http"
+	"strconv"
 
-	"github.com/gorilla/mux"
-	"gorm.io/gorm"
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
 )
 
-var instance *gorm.DB
+type IRewardController interface{}
 
-func CreateReward(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var reward entity.Reward
-	json.NewDecoder(r.Body).Decode(&reward)
-	instance.Create(&reward)
-	json.NewEncoder(w).Encode(reward)
+type rewardController struct {
+	rs rewardService.IRewardService
 }
 
-func FindAll(w http.ResponseWriter, r *http.Request) {
-	var rewards []entity.Reward
-	instance.Find(&rewards)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(rewards)
-}
-
-func FindRewardById(w http.ResponseWriter, r *http.Request) {
-	rewardId := mux.Vars(r)["id"]
-	if !checkIfRewardExists(rewardId) {
-		json.NewEncoder(w).Encode("Reward Not Found!")
-		return
+func NewRewardController(rs rewardService.IRewardService) rewardController {
+	return rewardController{
+		rs: rs,
 	}
-	var reward entity.Reward
-	instance.First(&reward, rewardId)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(reward)
 }
 
-func UpdateReward(w http.ResponseWriter, r *http.Request) {
-	rewardId := mux.Vars(r)["id"]
-	if !checkIfRewardExists(rewardId) {
-		json.NewEncoder(w).Encode("Reward Not Found!")
-		return
+func (rc rewardController) FindAllReward(c echo.Context) error {
+	data, err := rc.rs.FindAllReward(c.Request().Context())
+	if err != nil {
+		return responseErrorInternal(err, c)
 	}
-	var reward entity.Reward
-	instance.First(&reward, rewardId)
-	json.NewDecoder(r.Body).Decode(&reward)
-	instance.Save(&reward)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(reward)
+	return responseSuccess(data, c)
 }
 
-func DeleteReward(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	rewardId := mux.Vars(r)["id"]
-	if !checkIfRewardExists(rewardId) {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode("Reward Not Found!")
-		return
+func (rc rewardController) FindRewardById(c echo.Context) error {
+	param := c.Param("id")
+	id, err := strconv.ParseUint(param, 10, 64)
+	if err != nil {
+		return responseErrorParams(err, c)
 	}
-	var reward entity.Reward
-	instance.Delete(&reward, rewardId)
-	json.NewEncoder(w).Encode("Reward Deleted Successfully!")
+	data, err := rc.rs.FindRewardByID(c.Request().Context(), id)
+	if err != nil {
+		return responseErrorInternal(err, c)
+	}
+	return responseSuccess(data, c)
 }
 
-func checkIfRewardExists(rewardId string) bool {
-	var reward entity.Reward
-	instance.First(&reward, rewardId)
-	if reward.RewardID == 0 {
-		return false
+func (rc rewardController) CreateReward(c echo.Context) error {
+	var req dto.RewardRequest
+	c.Bind(&req)
+	validate := validator.New()
+	err := validate.Struct(req)
+	if err != nil {
+		return responseErrorValidator(err, c)
 	}
-	return true
+	err = rc.rs.CreateReward(c.Request().Context(), req)
+	if err != nil {
+		return responseErrorInternal(err, c)
+	}
+	return responseSuccess(echo.Map{
+		"status": "SUCCESS_INSERT_PRODUCT",
+	}, c)
+}
+func (rc rewardController) UpdateReward(c echo.Context) error {
+	param := c.Param("id")
+	id, err := strconv.ParseUint(param, 10, 64)
+	if err != nil {
+		return responseErrorParams(err, c)
+	}
+	var req dto.RewardRequest
+	c.Bind(&req)
+	err = rc.rs.UpdateReward(c.Request().Context(), req, id)
+	if err != nil {
+		return responseErrorInternal(err, c)
+	}
+	return responseSuccess(echo.Map{
+		"status": "SUCCESS_UPDATE_PRODUCT",
+	}, c)
+}
+
+func (rc rewardController) DeleteReward(c echo.Context) error {
+	param := c.Param("id")
+	id, err := strconv.ParseUint(param, 10, 64)
+	if err != nil {
+		return responseErrorParams(err, c)
+	}
+
+	err = rc.rs.DeleteReward(c.Request().Context(), id)
+	if err != nil {
+		return responseErrorInternal(err, c)
+	}
+	return responseSuccess(echo.Map{
+		"status": "SUCCESS_DELETE_PRODUCT",
+	}, c)
+}
+
+func responseErrorInternal(err error, c echo.Context) error {
+	errVal := response.ErrorResponseValue{
+		Key:   "error",
+		Value: err.Error(),
+	}
+	errRes := response.ErrorResponseData{errVal}
+	return echo.NewHTTPError(http.StatusInternalServerError,
+		response.NewBaseResponse(http.StatusInternalServerError,
+			http.StatusText(http.StatusInternalServerError),
+			errRes,
+			nil))
+}
+
+func responseSuccess(result interface{}, c echo.Context) error {
+	return c.JSON(http.StatusOK, response.NewBaseResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		nil,
+		result,
+	))
+}
+
+func responseErrorValidator(err error, c echo.Context) error {
+	errRes := response.ErrorResponseData{}
+	for _, val := range err.(validator.ValidationErrors) {
+		var errVal response.ErrorResponseValue
+		errVal.Key = val.StructField()
+		errVal.Value = val.Tag()
+		errRes = append(errRes, errVal)
+	}
+	return echo.NewHTTPError(http.StatusBadRequest,
+		response.NewBaseResponse(http.StatusBadRequest,
+			http.StatusText(http.StatusBadRequest),
+			errRes,
+			nil))
+}
+
+func responseErrorParams(err error, c echo.Context) error {
+	var errVal response.ErrorResponseValue
+	errVal.Key = "error"
+	errVal.Value = err.Error()
+
+	return echo.NewHTTPError(http.StatusBadRequest,
+		response.NewBaseResponse(http.StatusBadRequest,
+			http.StatusText(http.StatusBadRequest),
+			response.NewErrorResponseData(errVal),
+			nil))
 }
