4f7ee3728aeffb50492ce9815681f09e7f3864f8
Gede Gery Sastrawan <gerysastrawan123@gmail.com>
2022-12-14 20:01:27 +0800

    fix: response

--- internal/controller/redeem/redeem_controller.go
+++ internal/controller/redeem/redeem_controller.go
@@ -118,6 +118,9 @@ func (dc redeemController) CreateRedeem(c echo.Context) error {
 	}
 	err = dc.ds.CreateRedeem(c.Request().Context(), req, userId, userData)
 	if err != nil {
+		if err.Error() == "record not found" {
+			return response.ResponseSuccess(http.StatusOK, nil, c)
+		}
 		return response.ResponseError(http.StatusInternalServerError, err)
 	}
 	return response.ResponseSuccess(http.StatusCreated, echo.Map{
@@ -136,6 +139,9 @@ func (dc redeemController) UpdateRedeem(c echo.Context) error {
 
 	err = dc.ds.UpdateRedeem(c.Request().Context(), req, id)
 	if err != nil {
+		if err.Error() == "record not found" {
+			return response.ResponseSuccess(http.StatusOK, nil, c)
+		}
 		return response.ResponseError(http.StatusInternalServerError, err)
 	}
 	return response.ResponseSuccess(http.StatusOK, echo.Map{
@@ -151,6 +157,9 @@ func (dc redeemController) DeleteRedeem(c echo.Context) error {
 	}
 	err = dc.ds.DeleteRedeem(c.Request().Context(), id)
 	if err != nil {
+		if err.Error() == "record not found" {
+			return response.ResponseSuccess(http.StatusOK, nil, c)
+		}
 		return response.ResponseError(http.StatusInternalServerError, err)
 	}
 	return response.ResponseSuccess(http.StatusOK, echo.Map{
