43565063f341773747d3477f013efe36bcab264b
Gede Gery Sastrawan <gerysastrawan123@gmail.com>
2022-12-18 13:28:29 +0800

    fix: migration sql

--- database/migration/20221218034610_insert_reward_table.up.sql
+++ database/migration/20221218034610_insert_reward_table.up.sql
@@ -1,6 +1,6 @@
-INSERT INTO rewards(name,description,required_point,valid_until,category_id,created_at,updated_at,deleted_at)
-VALUES ('Pulsa 25.000','Khusus buat kamu yang pengen dapetin tambahan pulsa. Yuk buruan redeem diKoin kamu sekarang!',100,2022-12-31 24:00:00,1,now(),now(),null),
-('1 GB 1 Hari Pengguna By.U','Khusus buat kamu yang sudah kangen sama by.U. Yuk buruan redeem diKoin kamu sekarang!',200,2022-12-31 24:00:00,2,now(),now(),null),
-('Cashback 10.000 GoFood','Khusus buat kamu yang sudah kangen sama GoFood. Yuk buruan redeem diKoin kamu sekarang!',150,2022-12-31 24:00:00,3,now(),now(),null),
-('Modal Usaha 200.000','Khusus buat kamu yang pengen dapetin tambahan Modal Usaha. Yuk buruan redeem diKoin kamu sekarang!',1000,2022-12-31 24:00:00,4,now(),now(),null),
-('Voucher 50.000 Mobile Legend','Khusus buat kamu yang sudah kangen sama Voucher Mobile Legend. Yuk buruan redeem diKoin kamu sekarang!',400,2022-12-31 24:00:00,5,now(),now(),null);
+INSERT INTO rewards(name,description,required_point,valid_until,category_id,created_at,updated_at)
+VALUES ('Pulsa 25.000','Khusus buat kamu yang pengen dapetin tambahan pulsa. Yuk buruan redeem diKoin kamu sekarang!',100,"2022-12-31 23:59:00",1,now(),now()),
+('1 GB 1 Hari Pengguna By.U','Khusus buat kamu yang sudah kangen sama by.U. Yuk buruan redeem diKoin kamu sekarang!',200,"2022-12-31 23:59:00",2,now(),now()),
+('Cashback 10.000 GoFood','Khusus buat kamu yang sudah kangen sama GoFood. Yuk buruan redeem diKoin kamu sekarang!',150,"2022-12-31 23:59:00",3,now(),now()),
+('Modal Usaha 200.000','Khusus buat kamu yang pengen dapetin tambahan Modal Usaha. Yuk buruan redeem diKoin kamu sekarang!',1000,"2022-12-31 23:59:00",4,now(),now()),
+('Voucher 50.000 Mobile Legend','Khusus buat kamu yang sudah kangen sama Voucher Mobile Legend. Yuk buruan redeem diKoin kamu sekarang!',400,"2022-12-31 23:59:00",5,now(),now());
